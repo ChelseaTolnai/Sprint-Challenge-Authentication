fc14fd1363d63beee43b9b4d7a54223568d11c1c
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-05 19:23:08 -0500

    Implemented register function in /config/routes.js. Function first checks that username and password are in request. If confirmed, password is hashed and user is registered.

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+const db = require('../database/dbConfig');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +10,23 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ message: 'Error registering user. Username and password required.' });
+    return;
+  } else {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    try {
+      const [id] = await db('users').insert(user);
+      const registeredUser = await db('users').where({ id }).first();
+      res.status(201).json(registeredUser);
+    } catch (error) {
+      res.status(500).json({ message: 'Error registering user. Username may already be taken.', error });
+    }
+  }
 }
 
 function login(req, res) {
