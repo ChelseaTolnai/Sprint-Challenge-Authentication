60b870b61568827005322bc895dd35b7961f3f91
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-05 19:42:33 -0500

    Implemented login function in /config/routes.js. Function first check request has username and password. Once confirmed, retrieves user, compares hashed password, creates token, send token is response.

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 
 const { authenticate } = require('../auth/authenticate');
 const db = require('../database/dbConfig');
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'add a .env file to root of project with the JWT_SECRET variable';
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -29,8 +33,28 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ message: 'Error logging in. Username and password required.' });
+    return;
+  } else {
+    let { username, password } = req.body;
+    try {
+      const user = await db('users').where({ username }).first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = loginToken(user); 
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}.`
+        });
+      } else {
+        res.status(401).json({ message: 'Error logging in. Invalid Credentials.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error logging in.', error });
+    }
+  }
 }
 
 function getJokes(req, res) {
@@ -46,4 +70,15 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function loginToken(user) {
+  const payload = {
+    subject: user.id, 
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, jwtKey, options);
 }
